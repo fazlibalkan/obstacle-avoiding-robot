4acf706dcafc9620505a9b992f665b95234b8400
fazlibalkan <53044560+fazlibalkan@users.noreply.github.com>
2021-02-09 01:05:51 +0300

    bug fixes

--- obstacle-avoiding-robot.ino
+++ obstacle-avoiding-robot.ino
@@ -1,15 +1,17 @@
 #include <AFMotor.h>
-#include <Servo.h>
+#include <Servo.h> 
 
 #define echoPin A0
 #define pingPin A1
-#define speedVar 100
+#define speedVar 200     
 
+//Motors
 AF_DCMotor motor1(1);
 AF_DCMotor motor2(2);
 AF_DCMotor motor3(3);
 AF_DCMotor motor4(4);
 
+//An array of pointers to the motors
 AF_DCMotor* motors[4];
 
 Servo servo;
@@ -21,20 +23,24 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   servo.attach(10);  
-  servo.write(70);
+  servo.write(80);
 
+  //Making pointers to point to the motors
   motors[0] = &motor1;
   motors[1] = &motor2;
   motors[2] = &motor3;
   motors[3] = &motor4;
 
+  //Initializing the motors. Setting speed as 0 and releasing them.
   for (int i = 0; i < 4; i++) {
     motors[i]->setSpeed(0);
     motors[i]->run(RELEASE);
   }
-  
+
   pinMode(pingPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
+  //Starting the car
   MoveFORWARD();
 
 }
@@ -42,56 +48,73 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   distance = ReadUltraSonicInCm();
-  if (distance <= 20) {
+  
+  if (distance <= 15 && distance != 0) {
+    
     Stop();
     MoveBACWARD();
     Stop();
-    distanceLeft = Look(-70);
+    
+    distanceLeft = Look(-80);
     delay(200);
-    distanceRight = Look(70);
+    
+    distanceRight = Look(80);
 
     if (distanceLeft > distanceRight) {
       TurnLeft();
+      
       Stop();
       MoveFORWARD();
     } else {
       TurnRight();
+      
       Stop();
       MoveFORWARD();
     }
   }
+  delay(2);
   
   
 }
 
+/*
+ *Function for making robot go forward 
+ */
 void MoveFORWARD() {
   for (int i = 0; i < 4; i++){
     motors[i]->run(FORWARD);
   }
   
-  for (int i = 0; i < speedVar; i++) {
+  for (int i = 0; i < speedVar; i +=2) {
     for (int j = 0; j < 4; j++){
       motors[j]->setSpeed(i);
     }
-    delay(5);
+    delay(2);
   }
   
 }
 
+/*
+ *Function for making robot go backwards 
+ */
 void MoveBACWARD() {
   for (int i = 0; i < 4; i++){
     motors[i]->run(BACKWARD);
   }
   
-  for (int i = 0; i < speedVar; i++) {
+  for (int i = 0; i < speedVar; i +=2) {
     for (int j = 0; j < 4; j++){
       motors[j]->setSpeed(i);
     }
-    delay(5);
+    delay(2);
   }
+  delay(400);
   
 }
 
+/*
+ *Function for making robot stop 
+ */
 void Stop() {
   for (int i = 0; i < 4; i++){
     motors[i]->run(RELEASE);
@@ -99,34 +122,64 @@ void Stop() {
   delay(300);
 }
 
+/*
+ *Function for making robot look somewhere 
+ *It takes an angle to rotate its face
+ *negative angles mean left, positive angles mean right
+ */
 int Look(int angle) {
-  servo.write(angle + 90);
+  servo.write(angle + 80);
   delay(1000);
   int lookDistance = ReadUltraSonicInCm();
   delay(100);
 
-  servo.write(90);
+  servo.write(80);
   return lookDistance;
 }
 
+
+/*
+ *Functions for making robot turn
+ */
 void TurnRight() {
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  motor3.run(BACKWARD);
-  motor4.run(BACKWARD);
-  delay(500);
+  for (int i = 0; i < 2; i++){
+    motors[i]->run(BACKWARD);
+    motors[i+2]->run(FORWARD);
+  }
+  
+  for (int i = 0; i < speedVar; i +=2) {
+    for (int j = 0; j < 2; j++){
+      motors[j]->setSpeed(i);
+      motors[j+2]->setSpeed(i);
+    }
+    delay(2);
+  }
+  delay(700);
 }
 
 void TurnLeft() {
-  motor1.run(BACKWARD);
-  motor2.run(BACKWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);
-  delay(500);
+    for (int i = 0; i < 2; i++){
+    motors[i]->run(FORWARD);
+    motors[i+2]->run(BACKWARD);
+  }
+  
+  for (int i = 0; i < speedVar; i +=2) {
+    for (int j = 0; j < 2; j++){
+      motors[j]->setSpeed(i);
+      motors[j+2]->setSpeed(i);
+    }
+    delay(2);
+  }
+  delay(700 );
 }
 
+/*
+ * Function to read data from the pins of ultrasonic sensor in centimetre
+ */
+
 int ReadUltraSonicInCm(){
-  
+
+  //reading
   digitalWrite(pingPin, LOW);
   delayMicroseconds(2);
   digitalWrite(pingPin, HIGH);
@@ -135,7 +188,8 @@ int ReadUltraSonicInCm(){
   
   long duration = pulseIn(echoPin, HIGH, 25000);
 
-  int DistanceInCm = duration * 0.034 / 2;
+  //converting to cm
+  int DistanceInCm = duration / 29 /2;
   Serial.println(DistanceInCm);
   
   return DistanceInCm;
